7a2f94951b4031fefba00fc91145625ab2d92da1
Abstractmelon <abstractmelons@gmail.com>
2026-02-01 12:22:05 -0700

    Fix Name Popup

--- scripts/name_popup.gd
+++ scripts/name_popup.gd
@@ -3,9 +3,6 @@ extends AcceptDialog
 @onready var name_input: LineEdit = $MarginContainer/VBoxContainer/NameInput
 
 func _ready() -> void:
-	dialog_text = "Welcome to Machine Masquerade!\n\nPlease enter your designation:"
-	ok_button_text = "Confirm"
-	name_input.text = ""
 	name_input.grab_focus()
 
 func _on_confirmed() -> void:
