10d4228e4be906ecdb842185def2e89c62aaf65b
Abstractmelon <abstractmelons@gmail.com>
2026-01-31 20:26:48 -0700

    Pull spawn pos backwards

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -33,8 +33,6 @@ func randomize_target() -> void:
 	var spawner = get_tree().get_first_node_in_group("npc_spawner")
 	if spawner:
 		navigation_agent.set_target_position(spawner._get_random_spawn_position())
-	else:
-		navigation_agent.set_target_position(Vector3(randf_range(-15, 25), 0.5, randf_range(-30, 12)))
 
 func _apply_spawner_cosmetics() -> void:
 	if not has_meta("cosmetic_data"):
